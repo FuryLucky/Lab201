4956840025fa43ef8667c2a8e093c9a35a239d4b
Yannis <yannis1403@hotmail.fr>
2018-03-20 13:54:16 +0100

    Initial Commit

---
+++ views/front/auth.php
@@ -0,0 +1,18 @@
+<?php ob_start(); ?>
+
+    <!-- <link rel="stylesheet" href="/assets/stylesheets/nav.css"> -->
+
+<?php $stylesheetNav = ob_get_clean(); ?>
+
+<?php ob_start(); ?> <!-- Démarre le tampon de sortie -->
+
+<!-- ******************************************************** -->
+<!-- ******************** Page d'Accueil ******************** -->
+<!-- ******************************************************** -->
+
+<?php include  __DIR__ . '/../partials/nav.php'; ?>
+
+<!-- ob_get_clean -> récupere les octets dans la mémoire tampon et les mets dans une variable | echo pour l'afficher -->
+<?php $content = ob_get_clean(); ?>
+
+<?php include __DIR__ . '/../layouts/master.php' ; ?>
---
+++ views/layouts/master.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Afogec Competences</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Reset CSS -->
+    <link rel="stylesheet" href="/assets/stylesheets/reset.css">
+    <!-- CSS Perso -->
+</head>
+<body>
+    <?= $content; ?>
+</body>
+</html>
